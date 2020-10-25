f4cd2240475f1a74940d0c4d7979025bd95e0418
FlutLab.io <>
2020-10-25 22:59:35 +0000

    Initial commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:simple_login_page/login_page.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final avatar = Padding(
+      padding: EdgeInsets.all(20),
+        child: Hero(
+        tag: 'hero',
+        child: SizedBox(
+          height: 160,
+          child: Image.asset('assets/app.png'),
+        )
+      ),
+    );
+
+    final description = Padding(
+      padding: EdgeInsets.all(10),
+      child: RichText(
+        textAlign: TextAlign.justify,
+        text: TextSpan(
+          text: 'Hello there! Welcome to Alpha.',
+          style: TextStyle(color: Colors.black, fontSize: 30)
+        ),
+      ),
+    );
+
+    final buttonLogout = FlatButton(
+      child: Text('Logout', style: TextStyle(color: Colors.black87, fontSize: 16),),
+      onPressed: () {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+      }
+    );
+
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            children: <Widget>[
+              avatar,
+              description,
+              buttonLogout
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
